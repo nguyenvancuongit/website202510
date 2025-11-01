e4fbc3536a4f6cf5aa48af3d353e0eaea3e7bbb8
minhthai0124 <minhthai0124@gmail.com>
2025-11-01 20:23:36 +0700

    Fix tab 5

--- landing-page/src/app/solutions/enterprise/page.tsx
+++ landing-page/src/app/solutions/enterprise/page.tsx
@@ -18,7 +18,7 @@ const intros: SolutionIntroductionProps["introductions"] = [
   },
   {
     title: "人岗匹配",
-    description: "帮助企业在选人、用人、培养全流程中精准四配员工潜能与岗位需求",
+    description: "帮助企业在选人、用人、培养全流程中精准四配员工潜能与岗位需求。",
     image: "/images/solutions/enterprise/intro/intro-2.png",
     className: "bg-[linear-gradient(180deg,#BFF1FF_6.88%,rgba(191,241,255,0.20)_54.69%)]",
   },
@@ -34,7 +34,7 @@ const intros: SolutionIntroductionProps["introductions"] = [
 const advantages: SolutionAdvantagesSectionProps["advantages"] = [
   {
     title: "科学测评体系",
-    description: "基于数百万+学生及职场样本数据，确保评估信效度",
+    description: "基于数百万+基于数百万+学生及职场样本数据，确保评估信效度。",
     image: "/images/solutions/enterprise/advantages/advantages-1.png",
   },
   {
@@ -73,7 +73,7 @@ export default function EnterprisePage() {
         <TestimonialsSection
           title="方案背景"
           description={[
-            "数字经济时代正在势不可挡地向前发展，推动着企业数字化转型进程，也深度影响着HR领域，员工的能力优势、职业兴趣与内在动力直接影响组织效能与人才留存率，在此当景下，这些问题成为企业数字化转型进程中新的命题:",
+            "数字经济时代正在势不可挡地向前发展，推动着企业数字化转型进程，也深度影响着HR领域，员工的能力优势、职业兴趣与内在动力直接影响组织效能与人才留存率。在此背景下，这些问题成为企业数字化转型进程中新的命题：:",
             "①如何通过技术赋能未来人力资源管理?",
             "②如何通过科技让人力资源管理更有温度?",
             "③如何让人力资源管理适配数字经济时代的发展趋势?",
@@ -81,7 +81,7 @@ export default function EnterprisePage() {
         />
         <SolutionIntroduction
           title="方案介绍"
-          description="象导生涯企业人才发展解决方案整合职业测评、能力评估、发展建议三大模块，通过智能系统生成员工多维发展报告，包括职业兴趣(如霍兰德代码)、核心结合企业岗位需求，系统可自动推荐适配岗位或定制成长计划(如轮岗建议、培训课程)。能力(如领导力、创新思维)、价值观匹配度等关键指标。此外，方案支持团队分析功能，帮助管理者优化团队配置，识别潜在离职风险，并提供针对性干预策略(如职业锚定辅导、关键人才保留计划)。"
+          description="象导生涯企业人才发展解决方案整合职业测评、能力评估、发展建议三大模块，通过智能系统生成员工多维发展报告，包括职业兴趣（如霍兰德代码）、核心能力（如领导力、创新思维）、价值观匹配度等关键指标。结合企业岗位需求，系统可自动推荐适配岗位或定制成长计划（如轮岗建议、培训课程），并利用智能招聘面试舱协助企业筛选人才。此外，方案支持团队分析功能，帮助管理者优化团队配置，识别潜在离职风险，并提供针对性干预策略（如职业锚定辅导、关键人才保留计划）。(如霍兰德代码)、核心结合企业岗位需求，系统可自动推荐适配岗位或定制成长计划(如轮岗建议、培训课程)。能力(如领导力、创新思维)、价值观匹配度等关键指标。此外，方案支持团队分析功能，帮助管理者优化团队配置，识别潜在离职风险，并提供针对性干预策略(如职业锚定辅导、关键人才保留计划)。"
           introductions={intros}
         />
         <SolutionAdvantagesSection advantages={advantages} />
